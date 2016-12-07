96a892851c2b9316290ce176c64329eed59a4a66
ryandiew <ryan@trippie.co>
2016-12-06 19:54:13 -0500

    Initial Commit

---
+++ MappiePrototypeTests/MappiePrototypeTests.swift
@@ -0,0 +1,36 @@
+//
+//  MappiePrototypeTests.swift
+//  MappiePrototypeTests
+//
+//  Created by Ryan Diew on 12/6/16.
+//  Copyright © 2016 Ryan Diew. All rights reserved.
+//
+
+import XCTest
+@testable import MappiePrototype
+
+class MappiePrototypeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
